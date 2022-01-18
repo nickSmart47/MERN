4ef44f984d3a1e48590a01ad779d37becb8712ad
Nick Smart <nsmart147@gmail.com>
2022-01-17 20:23:30 -0500

    adjusted to use axios

--- 05-react_apis/pokemon-api/src/App.js
+++ 05-react_apis/pokemon-api/src/App.js
@@ -1,36 +1,39 @@
 import './App.css';
-// import Pokemon from './components/Pokemon'
 import PokemonDisplay from './components/PokemonDisplay'
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
   const [pokemon, setPokemon] = useState([]);
   let [showPokemon, setShowPokemon] = useState(false)
 
-  
-  useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=807/')
-      .then(response => response.json())
-      .then(response => setPokemon(response.results))
-  }, []);
+  // old way
+  // useEffect(() => {
+  //   fetch('https://pokeapi.co/api/v2/pokemon?limit=807/')
+  //     .then(response => response.json())
+  //     .then(response => setPokemon(response.results))
+  // }, []);
 
-  // console.log(showPokemon)
+  // new way using axios
+  useEffect(() => {
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=807/').then(response => { setPokemon(response.data.results) }, [])
+  })
+
 
   const toggleShowPokemon = () => {
-    if (showPokemon){
+    if (showPokemon) {
       setShowPokemon(false);
     }
-    else{
+    else {
       setShowPokemon(true);
 
     }
-    // console.log(showPokemon)
   }
   return (
     <div className="App container">
-      <button onClick = {toggleShowPokemon} className="btn btn-primary mt-5">Show/Hide Pokemon</button>
+      <button onClick={toggleShowPokemon} className="btn btn-primary mt-5">Show/Hide Pokemon</button>
       <div>
-        {showPokemon? <PokemonDisplay pokemon = {pokemon}></PokemonDisplay> : null}
+        {showPokemon ? <PokemonDisplay pokemon={pokemon}></PokemonDisplay> : null}
       </div>
     </div>
   );
