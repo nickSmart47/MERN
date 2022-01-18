af00299a67672b89f0b6802a2520c253f3b382f3
Nick Smart <nsmart147@gmail.com>
2022-01-18 10:41:59 -0500

    initial commit, seems to be working!

---
+++ 00-algos/strToWordArr.js
@@ -0,0 +1,28 @@
+function strToWordArr(str) {
+    let result = [];
+    let word = ""
+    for (let i = 0; i <= str.length; i++) {
+        if (str[i] !== " " && str[i - 1] == " ") {
+            word += str[i]
+        }
+        else if (str[i] !== " " && i == str.length - 1) {
+            word += str[i]
+            result.push(word)
+        }
+        else if (str[i] !== " ") {
+            word += str[i]
+        }
+        else if (str[i] == " ") {
+            if (word !== "") {
+                result.push(word)
+                word = ""
+            }
+        }
+    }
+    return result;
+}
+
+console.log(strToWordArr("  Welcome    to week    two!!!!")) //["Welcome", "to", "week", "two!"]
+// console.log(strToWordArr("  Welcome  to week    two!   "))
+
+
