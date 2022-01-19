905963f92f36a809c193bf51738801411021c080
Nick Smart <nsmart147@gmail.com>
2022-01-19 10:14:03 -0500

    renamed filename

---
+++ 00-algos/rotateStr.js
@@ -0,0 +1,23 @@
+//create a function that accepts a string and a number as an input, and the function will return the string rotated by the amount in the number input
+
+
+function rotateStr(str, num){
+
+}
+
+
+console.log(rotateStr("abcde", 2)) //deabc
+console.log(rotateStr("abcdefg", 5)) //cdefgab
+console.log(rotateStr("abcdefg", 10)) //efgabcd
+
+
+
+
+
+//given two strings, return true if they are rotations of one another. return false if they are not
+function isRotation(str1,str2){
+
+}
+
+console.log(isRotation("abcde", "deabc")) //true
+console.log(isRotation("abcde", "edacb")) //false
