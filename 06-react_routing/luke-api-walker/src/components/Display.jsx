3abaf151e2e53d40a0155b6f06e435475e9d44b3
Nick Smart <nsmart147@gmail.com>
2022-01-19 13:19:09 -0500

    added link to homeworld!

--- 06-react_routing/luke-api-walker/src/components/Display.jsx
+++ 06-react_routing/luke-api-walker/src/components/Display.jsx
@@ -30,15 +30,35 @@ const StarWarsDisplay = (props, history) => {
                     droids()
                 });
         }
+        console.log(homeworldData)
     }
 
     const droids = () => {
         setTheseArentTheDroids(true)
     }
 
+    const getHomeworldLink = () => {
+        if (data.homeworld){
+            let homeworldLink = data.homeworld
+                console.log(homeworldLink)
+                let homeworldNumber = ''
+                for (let i = 0; i< homeworldLink.length; i++){
+                    if (isNaN(homeworldLink[i])){
+                        continue
+                    }
+                    else{
+                        homeworldNumber += homeworldLink[i]
+                    }
+                }
+                homeworldLink = `/planets/${homeworldNumber}`
+                // console.log(homeworldLink)
+                return homeworldLink
+
+        }
+    }
+
     if (data) {
         if (category === "people") {
-
             return (
                 <div className="p-2 container d-flex flex-column">
                     <h3>{data.name}</h3>
@@ -46,7 +66,7 @@ const StarWarsDisplay = (props, history) => {
                     <p><strong>Mass: </strong> {data.mass}</p>
                     <p><strong>Hair Color: </strong> {data.hair_color}</p>
                     <p><strong>Skin Color: </strong> {data.skin_color}</p>
-                    <p><strong>Homeworld: </strong> {homeworldData.name}</p>
+                    <p><strong>Homeworld: </strong> {homeworldData.name} <a href={getHomeworldLink()}>Link</a></p>
                 </div>
             )
         }
