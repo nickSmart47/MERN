4744732e5b2aaa92b4625e117f63b58b5cadd084
Nick Smart <nsmart147@gmail.com>
2022-01-24 12:24:07 -0500

    express

---
+++ 08-mongodb_mongoose/01-ninjas_belt_reviewer/server.js
@@ -0,0 +1,24 @@
+/*
+commands for starting new fullstack app!
+npm init -y
+npm install mongoose express
+touch server.js
+*/
+
+const express = require('express'); // import express
+const mongoose = require('mongoose'); // import mongoose
+
+const app = express(); // initialize express
+const port = 8000; // specify port in variable
+
+
+app.get("/api/hello", (req,res)=>{
+    res.json({msg: "Hello mongoose!"})
+})
+
+
+
+
+
+
+app.listen( port, () => console.log(`Listening on port: ${port}`))
