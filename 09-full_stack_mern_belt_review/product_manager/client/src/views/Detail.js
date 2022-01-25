9e0631e8dcfd600d752ca77b9a1650afa2e18796
Nick Smart <nsmart147@gmail.com>
2022-01-25 17:31:12 -0500

    added delete button with redirect on detail page

--- 09-full_stack_mern_belt_review/product_manager/client/src/views/Detail.js
+++ 09-full_stack_mern_belt_review/product_manager/client/src/views/Detail.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useHistory } from "react-router-dom";
     
 const Detail = (props) => {
     const [product, setProduct] = useState({})
     const { id } = useParams();
+    const history = useHistory();
     
     useEffect(() => {
         axios.get('http://localhost:8000/api/products/' +id)
@@ -12,6 +13,20 @@ const Detail = (props) => {
         // is this returns an array of product objects (even though said array only has one element..)
         .catch(err => console.error(err));
     }, []);
+
+
+    // const { removeFromDom } = props;
+
+    const deleteProduct = (productId) => {
+        axios.delete(`http://localhost:8000/api/products/${productId}`)
+            .then(res => {
+                // removeFromDom(productId)
+                console.log(res)
+            })
+            .catch(err => console.error(err))
+
+            history.push('/products')
+    }
     
     // console.log(product)
     return (
@@ -19,7 +34,12 @@ const Detail = (props) => {
             <h2>{product.title}</h2>
             <p><strong>Price:</strong> ${product.price}</p>
             <p><strong>Description:</strong> {product.description}</p>
-            <Link to={`/products/${product._id}/edit/`}><button className="btn btn-primary">Edit</button></Link>
+            <div className = "d-flex align-items-center gap-3">
+                <Link to={`/products/${product._id}/edit/`}><button className="btn btn-primary">Edit</button></Link>
+                <button className="btn btn-danger" onClick={(e) => { deleteProduct(product._id) }}>
+                            Delete
+                        </button>
+            </div>
         </div>
     )
 }
